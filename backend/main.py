53c344176b29d62c4609c689fd1e1807f2e04b2c
ranikumari01936-bit <ranikumari01936@gmail.com>
2026-08-20 21:03:09 +0530

    Update backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import models
 from database import engine, get_db
 from schemas import VendorCreate, VendorResponse
+
+class SalesInput(BaseModel):
+    yesterday_sales: float
+
+class InventoryInput(BaseModel):
+    forecast_demand: float
+    current_stock: float
+    safety_stock: float = 5
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -72,3 +81,40 @@ def get_vendors(db: Session = Depends(get_db)):
     vendors = db.query(models.Vendor).all()
 
     return vendors
+
+@app.post("/forecast")
+def demand_forecast(sales: SalesInput):
+    forecast_demand = round(sales.yesterday_sales * 1.1, 2)
+
+    return {
+        "yesterday_sales": sales.yesterday_sales,
+        "forecast_demand": forecast_demand,
+        "message": "Demand forecast generated successfully."
+    }
+
+
+@app.post("/inventory")
+def inventory_planner(inventory: InventoryInput):
+    required_stock = (
+        inventory.forecast_demand
+        + inventory.safety_stock
+    )
+
+    recommended_purchase = max(
+        0,
+        required_stock - inventory.current_stock
+    )
+
+    message = (
+        "Additional stock is recommended for today's demand."
+        if recommended_purchase > 0
+        else "Your current stock is sufficient for today's demand."
+    )
+
+    return {
+        "forecast_demand": inventory.forecast_demand,
+        "current_stock": inventory.current_stock,
+        "safety_stock": inventory.safety_stock,
+        "recommended_purchase": round(recommended_purchase, 2),
+        "message": message
+    }
